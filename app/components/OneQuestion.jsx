22ad7dea0f7b42fc074890b96da8dc51d1fbde2d
Brandon Culp <bculp513@gmail.com>
2016-11-11 23:55:19 -0500

    FINALLY FIXED EVERYTHING. ANNYANG IS LISTENING TO MY COMMANDS

--- app/components/OneQuestion.jsx
+++ app/components/OneQuestion.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import axios from 'axios';
 
 const annyang = require('../../public/annyang_source_code');
+
 let helloFunction = function() {
 	alert('hello!!')
 }
+
+let testFunction = function() {
+	console.log('testing 1 2 3')
+}
 // // //annyang commands
-let command = {'test': helloFunction};
+let command = {'test': testFunction};
+let command2 = {'hello': helloFunction}
 annyang.addCommands(command)
+annyang.addCommands(command2)
+
 
 //setup outline for 1 component or 1 question then will just render one of these
 //components for each question
@@ -23,6 +31,7 @@ export default class OneQuestion extends React.Component {
 				<h1>hello</h1>
 				
 					{console.log(annyang.isListening())}
+					{annyang.debug()}
 					{annyang.start()}
 					
 					{console.log(annyang.isListening())}
